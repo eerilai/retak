675030fe5a39894ff7688084d510ca35940b0de5
Evan Powell <evan@Evans-MacBook-Pro.local>
2018-05-03 20:23:21 -0500

    Add dotenv module

--- server/index.js
+++ server/index.js
@@ -3,11 +3,23 @@ const session = require('express-session');
 const passport = require('passport');
 const path = require('path');
 
+require('dotenv').config();
 // Feel free to move or copy/paste the line below 
 // where db queries are required
 const db = require('../database');
 
 const app = express();
+
+console.log('session secret: ' + process.env.SESSION_SECRET);
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 10 * 1000
+  }
+}));
 
 app.use('/', express.static(path.join(__dirname, '../client/dist')));
 
