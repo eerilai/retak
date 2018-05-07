02f217dea93bc3b6a3b98eaa2ba8ad87ef9b0a26
ALB24 <universidad.aless@gmail.com>
2018-05-07 14:30:29 -0500

    added redux-promise dependency

--- client/src/actions/sampleAction.js
+++ client/src/actions/sampleAction.js
@@ -1,6 +1,8 @@
+export const CHANGE_STORE = 'CHANGE_STORE';
+
 export const actionFunc = () => {
 	return {
-		type: 'CHANGE_STORE',
+		type: CHANGE_STORE,
 		payload: {
 			newState: 'New state',
 			anotherState: 'something'
