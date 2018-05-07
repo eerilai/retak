cb6db8169e17d163e3d4f1076367d5664c14486b
shuchen <shuchenowl@gmail.comm>
2018-05-07 14:41:01 -0500

    fixed router

--- server/index.js
+++ server/index.js
@@ -1,42 +1,53 @@
-const express = require('express');
-const session = require('express-session');
-const passport = require('passport');
-const path = require('path');
-const bodyParser = require('body-parser');
+const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+const path = require("path");
+const bodyParser = require("body-parser");
 
-require('dotenv').config();
+require("dotenv").config();
 
-const db = require('../database');
-const authRoutes = require('./routes/authRoutes');
+const db = require("../database");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
 app.use(bodyParser());
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: true,
-  cookie: {
-    maxAge: 24 * 60 * 60 * 1000
-  }
-}));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 24 * 60 * 60 * 1000
+    }
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use('/auth', authRoutes);
+app.use("/auth", authRoutes);
 
 // Implement authorization check for relevant requests, ie profile, logout, etc
-const authCheck = ((req, res, next) => {
-  if(!req.user) {
-    res.redirect('/');
+const authCheck = (req, res, next) => {
+  if (!req.user) {
+    res.redirect("/");
   } else {
     next();
   }
+};
+//app.use("/", express.static(path.join(__dirname, "../client/dist/")));
+
+app.get("/bundle.js", (req, res) => {
+  console.log("getting bundle");
+  res.sendFile(path.join(__dirname, "../client/dist/bundle.js"));
 });
-
-app.use('/', express.static(path.join(__dirname, '../client/dist')));
-
+app.get("/*", (req, res) => {
+  console.log("Wildcard route, sending back index.html :)");
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => { console.log(`Listening on port ${PORT}`); });
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
