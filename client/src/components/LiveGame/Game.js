40bd0b5b3c5b8be303346b99d8fe1deb34077e18
eerilai <eerilaie@gmail.com>
2018-05-08 17:17:49 -0500

    Move board creation inside of game class
    idk why this was outside of it
    Also slightly changed max width on board to fix minor
    render error

--- client/src/components/LiveGame/Game.js
+++ client/src/components/LiveGame/Game.js
@@ -1,28 +1,30 @@
 import { pieceCount } from './gameUtil';
 import Stack from './Stack';
 
-const createBoard = (size) => {
-  const board = [];
-  for (let row = 0; row < size; row += 1) {
-    board[row] = [];
-    for (let col = 0; col < size; col += 1) {
-      board[row][col] = new Stack(this, row, col);
-    }
-  }
-  return board;
-};
-
 class Game {
   constructor(size, player1 = 'p1', player2 = 'p2') {
-    this.size = size;
     this.first = player1;
     this.second = player2;
-    this.board = createBoard(size);
+    this.size = size;
+    this.board = [];
+    this.squares = {};
+    this.createBoard(size);
     this.toPlay = 1;
     this.pieces = {
       1: { ...pieceCount[size] }, // Props ['F'], ['C'], ['Total']
       2: { ...pieceCount[size] },
     };
+  }
+
+  createBoard(size) {
+    for (let row = 0; row < size; row += 1) {
+      this.board[row] = [];
+      for (let col = 0; col < size; col += 1) {
+        const stack = new Stack(this, row, col);
+        this.board[row][col] = stack;
+        this.squares[stack.coord] = stack;
+      }
+    }
   }
 }
 
