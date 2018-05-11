42e4935d462f54925c32bab8c23c73ac756c34b4
Evan Powell <evan@Evans-MacBook-Pro.local>
2018-05-11 08:46:19 -0500

    Clean up code
    - Get rid of unnecessary spaces in dist index
    - Delete gameRoutes file and import lines in server

--- client/src/components/Home/index.jsx
+++ client/src/components/Home/index.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
 import Lobby from "./lobby";
+
 const Home = props => (
   <div className="main">
     <div className="home home-grid">
