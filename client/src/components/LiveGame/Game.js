ca7a9e268321f6d9244d784feb8261e07ab3f7cc
eerilai <eerilaie@gmail.com>
2018-05-11 17:17:29 -0500

    Prevent moves from being made after victor is set

--- client/src/components/LiveGame/Game.js
+++ client/src/components/LiveGame/Game.js
@@ -60,98 +60,100 @@ class Game {
     const stack = this.squares[coord];
     const { isEmpty } = stack;
 
-    if (!this.isMoving) {
-      // Place a Stone
-      if (isEmpty) {
-        if (this.pieces[this.toPlay].F !== 0) {
-          stack.place(this.toPlay, stone);
-          if (stone === 'C') {
-            this.pieces[this.toPlay].C -= 1;
-          } else {
-            this.pieces[this.toPlay].F -= 1;
+    if (this.victor === 0) {
+      if (!this.isMoving) {
+        // Place a Stone
+        if (isEmpty) {
+          if (this.pieces[this.toPlay].F !== 0) {
+            stack.place(this.toPlay, stone);
+            if (stone === 'C') {
+              this.pieces[this.toPlay].C -= 1;
+            } else {
+              this.pieces[this.toPlay].F -= 1;
+            }
+            this.checkRoads();
+            this.toPlay = (this.toPlay === 1) ? 2 : 1;
           }
-          this.checkRoads();
-          this.toPlay = (this.toPlay === 1) ? 2 : 1;
+        // Start a move
+        } else if (!isEmpty && (stack.owner === this.toPlay)) {
+          this.moveStack = [...stack.stack];
+          this.toMove.stack = stack.stack.splice(0, this.size);
+          this.toMove.stone = stack.stone;
+          this.toMove.coord = coord;
+          stack.stone = '';
+          stack.owner = stack.stack[0] || 0;
+          stack.isEmpty = !stack.stack.length;
+          this.isMoving = true;
+          this.moveOrigin = this.squares[coord];
+          this.moveOrigin.validMove = true;
+          Object.keys(stack.neighbors)
+            .forEach((dir) => {
+              if (stack.neighbors[dir].stone === '') {
+                stack.neighbors[dir].validMove = true;
+              } else if (stack.neighbors[dir].stone === 'S' &&
+                         this.toMove.stone === 'C' &&
+                         this.toMove.stack.length === 1) {
+                stack.neighbors[dir].validMove = true;
+              }
+            });
         }
-      // Start a move
-      } else if (!isEmpty && (stack.owner === this.toPlay)) {
-        this.moveStack = [...stack.stack];
-        this.toMove.stack = stack.stack.splice(0, this.size);
-        this.toMove.stone = stack.stone;
+      // Continue Movement
+      } else if (this.isMoving &&
+                 stack.stone === '' &&
+                 stack.validMove === true) {
+        this.setMoveDir(stack);
         this.toMove.coord = coord;
-        stack.stone = '';
-        stack.owner = stack.stack[0] || 0;
-        stack.isEmpty = !stack.stack.length;
-        this.isMoving = true;
-        this.moveOrigin = this.squares[coord];
-        this.moveOrigin.validMove = true;
-        Object.keys(stack.neighbors)
-          .forEach((dir) => {
-            if (stack.neighbors[dir].stone === '') {
-              stack.neighbors[dir].validMove = true;
-            } else if (stack.neighbors[dir].stone === 'S' &&
+        if (this.moveDir !== '') {
+          this.moveOrigin.validMove = false;
+          Object.keys(this.moveOrigin.neighbors)
+            .forEach((dir) => { this.moveOrigin.neighbors[dir].validMove = false; });
+          if (Object.prototype.hasOwnProperty.call(stack.neighbors, this.moveDir)) {
+            if (stack.neighbors[this.moveDir].stone === '') {
+              stack.neighbors[this.moveDir].validMove = true;
+            } else if (stack.neighbors[this.moveDir].stone === 'S' &&
                        this.toMove.stone === 'C' &&
-                       this.toMove.stack.length === 1) {
-              stack.neighbors[dir].validMove = true;
+                       this.toMove.stack.length === 2) {
+              stack.neighbors[this.moveDir].validMove = true;
             }
-          });
-      }
-    // Continue Movement
-    } else if (this.isMoving &&
-               stack.stone === '' &&
-               stack.validMove === true) {
-      this.setMoveDir(stack);
-      this.toMove.coord = coord;
-      if (this.moveDir !== '') {
-        this.moveOrigin.validMove = false;
-        Object.keys(this.moveOrigin.neighbors)
-          .forEach((dir) => { this.moveOrigin.neighbors[dir].validMove = false; });
-        if (Object.prototype.hasOwnProperty.call(stack.neighbors, this.moveDir)) {
-          if (stack.neighbors[this.moveDir].stone === '') {
-            stack.neighbors[this.moveDir].validMove = true;
-          } else if (stack.neighbors[this.moveDir].stone === 'S' &&
-                     this.toMove.stone === 'C' &&
-                     this.toMove.stack.length === 2) {
-            stack.neighbors[this.moveDir].validMove = true;
           }
+          stack.validMove = true;
+          this.step = stack;
         }
-        stack.validMove = true;
-        this.step = stack;
-      }
-      stack.place(this.toMove.stack.pop());
-      if (this.toMove.stack.length === 1 && this.toMove.stone === 'C') {
-        Object.keys(this.moveOrigin.neighbors)
-          .forEach((dir) => {
-            if (stack.neighbors[dir].stone === 'S') {
-              stack.neighbors[dir].validMove = true;
-            }
-          });
-      }
-      if (!this.toMove.stack.length) {
-        stack.stone = this.toMove.stone;
-        this.toMove = {};
-        this.isMoving = false;
-        this.moveOrigin.validMove = false;
+        stack.place(this.toMove.stack.pop());
+        if (this.toMove.stack.length === 1 && this.toMove.stone === 'C') {
+          Object.keys(this.moveOrigin.neighbors)
+            .forEach((dir) => {
+              if (stack.neighbors[dir].stone === 'S') {
+                stack.neighbors[dir].validMove = true;
+              }
+            });
+        }
+        if (!this.toMove.stack.length) {
+          stack.stone = this.toMove.stone;
+          this.toMove = {};
+          this.isMoving = false;
+          this.moveOrigin.validMove = false;
+          Object.keys(this.squares)
+            .forEach((c) => { this.squares[c].validMove = false; });
+          if (this.moveDir !== '') {
+            this.checkRoads();
+            this.toPlay = (this.toPlay === 1) ? 2 : 1;
+          }
+          this.moveDir = '';
+        }
+      // Wallsmash
+      } else if (this.isMoving &&
+                 stack.stone === 'S' &&
+                 this.toMove.stone === 'C' &&
+                 this.toMove.stack.length === 1) {
+        stack.place(this.toMove.stack.pop(), 'C');
+        this.toMove.coord = '';
         Object.keys(this.squares)
           .forEach((c) => { this.squares[c].validMove = false; });
-        if (this.moveDir !== '') {
-          this.checkRoads();
-          this.toPlay = (this.toPlay === 1) ? 2 : 1;
-        }
-        this.moveDir = '';
+        this.isMoving = false;
+        this.checkRoads();
+        this.toPlay = (this.toPlay === 1) ? 2 : 1;
       }
-    // Wallsmash
-    } else if (this.isMoving &&
-               stack.stone === 'S' &&
-               this.toMove.stone === 'C' &&
-               this.toMove.stack.length === 1) {
-      stack.place(this.toMove.stack.pop(), 'C');
-      this.toMove.coord = '';
-      Object.keys(this.squares)
-        .forEach((c) => { this.squares[c].validMove = false; });
-      this.isMoving = false;
-      this.checkRoads();
-      this.toPlay = (this.toPlay === 1) ? 2 : 1;
     }
   }
 
@@ -164,6 +166,7 @@ class Game {
       if ((checkNS && square.edges.includes('+')) ||
           (checkEW && square.edges.includes('>'))) {
         this.victor = p;
+        this.winType = 'R';
       } else {
         checked.push(square.coord);
         const up = square.neighbors['+'];
