091b615fba82dc6d61a792c82cade199f23d53eb
shuchen <shuchenowl@gmail.comm>
2018-05-12 20:25:49 -0500

    chat box

--- client/src/components/LiveGame/chat.jsx
+++ client/src/components/LiveGame/chat.jsx
@@ -1,47 +1,66 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import { Link, withRouter } from "react-router-dom";
 class Chat extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      message: '',
+      message: "",
       messages: [],
-      typing: ''
+      typing: ""
     };
 
-
-    const { socket } = props
-    socket.on('typing', function(data) {
-      self.setState({ typing: data.author + ' is typing...' });
+    const { socket } = props;
+    socket.on("typing", function(data) {
+      self.setState({ typing: data.author + " is typing..." });
     });
 
-    socket.on('chat', function(data) {
+    socket.on("chat", function(data) {
+      console.log("data", data);
+
       addMessage(data);
     });
 
     const addMessage = data => {
-      this.setState({ messages: [...this.state.messages, data], typing: '' });
+      console.log("adding message", data);
+      this.setState({ messages: [...this.state.messages, data], typing: "" });
     };
 
     this.sendMessage = ev => {
+      const roomID = this.props.location.pathname.split("/").pop();
+      console.log("roomId", roomID);
       ev.preventDefault();
-      socket.emit('chat', {
+      socket.emit("chat", {
         author: this.props.username,
-        message: this.state.message
+        message: this.state.message,
+        room: roomID
       });
-      this.setState({ message: '' });
+      this.setState({ message: "" });
     };
 
     this.handleTyping = () => {
-      socket.emit('typing', {
+      socket.emit("typing", {
         author: this.props.username
       });
+    };
+    this.handleTak = ev => {
+      const roomID = this.props.location.pathname.split("/").pop();
+      console.log("roomId", roomID);
+      ev.preventDefault();
+      socket.emit("chat", {
+        author: this.props.username,
+        message: "TAK",
+        room: roomID
+      });
+      this.setState({ message: "" });
     };
   }
   render() {
     return (
       <div id="chat">
+        <button onClick={this.handleTak} id="tak">
+          Tak
+        </button>
         <div id="chat-window">
           <div id="output">
             {this.state.messages.map(message => {
@@ -83,4 +102,4 @@ class Chat extends Component {
     );
   }
 }
-export default Chat;
+export default withRouter(Chat);
