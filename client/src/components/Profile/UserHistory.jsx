6faf39811b6708e28a03b54d56330f5aec0f399f
ALB24 <universidad.aless@gmail.com>
2018-05-17 21:14:09 -0500

    Display Game History for each Profile Route

--- client/src/components/Profile/UserHistory.jsx
+++ client/src/components/Profile/UserHistory.jsx
@@ -5,8 +5,7 @@ const UserHistory = ({ userHistory }) => {
   console.log("userHistory", userHistory)
   const eachUserGame = userHistory.map((user) => {
     const losses = user.ranked_games - user.ranked_wins;
-    const time = user.createdAt.slice(0, 19);
-    console.log(time)
+    const time = moment(user.createdAt.slice(0, 19)).format('MMMM Do YYYY, h:mm:ss a');
     let typeOfWin = '';
     if(user.win_type === 'R') {
       typeOfWin = 'Road Win';
@@ -14,8 +13,8 @@ const UserHistory = ({ userHistory }) => {
     // if(){}
     
     return (
-      <div key={user.id}>
-        <div>Played On: {moment(time).format('MMMM Do YYYY, h:mm:ss a')}</div>
+      <div key={user.id} className="eachGame">
+        <div>Played On: {time}</div>
         <div>{user.player1} vs {user.player2}</div>
         <div>Victor: {user.victor}</div>
         <div>{typeOfWin}</div>
