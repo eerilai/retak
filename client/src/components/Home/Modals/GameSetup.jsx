af853567c8e48b222ab5b87a9247f473c99e1900
eerilai <eerilaie@gmail.com>
2018-05-19 10:27:58 -0500

    Add time control field to create game modal

--- client/src/components/Home/Modals/GameSetup.jsx
+++ client/src/components/Home/Modals/GameSetup.jsx
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
 import {
-  Input,
   Button,
-  Header,
   Modal,
-  Icon,
   Form,
   Select,
-  Transition,
-  Checkbox
+  Checkbox,
 } from 'semantic-ui-react';
 
 class GameSetup extends Component {
@@ -17,12 +13,14 @@ class GameSetup extends Component {
     this.state = {
       boardSize: 0,
       isPrivate: false,
-      roomName: ''
-    }
+      roomName: '',
+      time: false,
+    };
 
     this.handleBoardSizeChange = this.handleBoardSizeChange.bind(this);
     this.handlePrivacyChange = this.handlePrivacyChange.bind(this);
     this.handleRoomNameChange = this.handleRoomNameChange.bind(this);
+    this.handleTimeChange = this.handleTimeChange.bind(this);
   }
 
   handleBoardSizeChange(e, { value }) {
@@ -35,6 +33,18 @@ class GameSetup extends Component {
     this.setState({
       isPrivate: !this.state.isPrivate
     });
+  }
+
+  handleTimeChange(e, { value }) {
+    if (value === '0') {
+      this.setState({
+        time: -1,
+      });
+    } else {
+      this.setState({
+        time: Number(value) * 60,
+      });
+    }
   }
 
   handleRoomNameChange(e, { value }) {
@@ -50,27 +60,40 @@ class GameSetup extends Component {
       { key: '6', text: '6', value: '6' },
       { key: '5', text: '5', value: '5' },
       { key: '4', text: '4', value: '4' },
-      { key: '3', text: '3', value: '3' }
+      { key: '3', text: '3', value: '3' },
+    ];
+    const times = [
+      { key: '2', text: '15', value: '15' },
+      { key: '3', text: '10', value: '10' },
+      { key: '4', text: '5', value: '5' },
+      { key: '5', text: '3', value: '3' },
     ];
     const isFriendly = this.props.gameType === 'friend';
-    const { boardSize, isPrivate, roomName } = this.state;
+    const { boardSize, isPrivate, roomName, time } = this.state;
     return (
         <Modal
           open={this.props.modalView === 'GameSetup'}
-          size={"tiny"}
+          size="tiny"
           onClose={() => this.props.changeView('')}
           dimmer={false}
           closeIcon
         >
           <Modal.Header>GameSetup</Modal.Header>
           <Modal.Content>
-            <Form size={"tiny"} key={"small"}>
+            <Form size="tiny" key="small">
               <Form.Field
                 control={Select}
                 label="Board Size"
                 options={options}
                 placeholder="Board Size"
                 onChange={this.handleBoardSizeChange}
+              />
+              <Form.Field
+                control={Select}
+                placeholder="Time Control"
+                label="Time Control"
+                options={times}
+                onChange={this.handleTimeChange}
               />
               <Form.Field
                 control={Checkbox}
@@ -93,7 +116,9 @@ class GameSetup extends Component {
               size="large"
               labelPosition="right"
               content="New Game"
-              onClick={() => this.props.handleCreateGame(boardSize, isFriendly, isPrivate, roomName)}
+              onClick={() => {
+                this.props.handleCreateGame(boardSize, time, isFriendly, isPrivate, roomName);
+              }}
             />
           </Modal.Actions>
         </Modal>
