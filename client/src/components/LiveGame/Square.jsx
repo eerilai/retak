379726c72f96eb6c81e9a0e4e3e7291acdb35eef
eerilai <eerilaie@gmail.com>
2018-05-22 15:15:52 -0500

    feat(Stones): Improve CSS rendering / positioning of special stones

--- client/src/components/LiveGame/Square.jsx
+++ client/src/components/LiveGame/Square.jsx
@@ -6,6 +6,8 @@ const Square = ({ game, row, col, handleSquareClick }) => {
   const color = (row % 2 !== col % 2) ? '#DEE3E6' : '#8CA2AD';
   const coord = convertCoord([col, row]);
   const stack = game.board[col][row];
+  let top = '';
+  if (stack.stack.length > 1) top = 'top';
   const valid = stack.validMove ? 'valid' : '';
   const origin = stack === game.moveOrigin ? 'origin' : '';
 
@@ -19,7 +21,7 @@ const Square = ({ game, row, col, handleSquareClick }) => {
     width: `${stoneSize}px`,
     height: `${stoneSize}px`,
   };
-  const topStyle = {
+  const topStackStyle = {
     ...stoneStyle,
     'min-height': stoneSize,
   };
@@ -45,7 +47,7 @@ const Square = ({ game, row, col, handleSquareClick }) => {
         <div className="selected" style={stoneStyle}>
           {game.toMove.stack.map((x, i) =>
              (i === 0 ?
-               <div className={`p${x} ${game.toMove.stone} stone top`} style={{ ...stoneStyle, 'z-index': '999' }}>
+               <div className={`p${x} ${game.toMove.stone} stone top`} style={{ ...stoneStyle, 'z-index': '10' }}>
                  {/* <p className="non-flat">{game.toMove.stone}</p> */}
                </div> :
                <div
@@ -53,7 +55,7 @@ const Square = ({ game, row, col, handleSquareClick }) => {
                  style={
                   {
                     ...stoneStyle,
-                    'z-index': `${999 - i}`,
+                    'z-index': `${10 - i}`,
                     'margin-top': '-65%',
                   }}
                />))
@@ -76,15 +78,15 @@ const Square = ({ game, row, col, handleSquareClick }) => {
           {renderSelected()}
           {stack.stack.map((x, i) =>
              (i === 0 ?
-               <div className={`p${x} ${stack.stone} stone top`} style={topStyle}>
+               <div className={`p${x} ${stack.stone} stone ${top}`} style={topStackStyle}>
                  {/* <p className="non-flat">{` ${stack.stone} `}</p> */}
                </div> :
-               <div className={`p${x} stone captive`} style={{ ...stoneStyle, 'z-index': `${999 - i}` }} />))
+               <div className={`p${x} stone captive`} style={{ ...stoneStyle, 'z-index': `${10 - i}` }} />))
           }
         </div>
       );
     } else if (stack.stack.length > maxStack) {
-      const top = stack.stack.slice(0, maxStack);
+      const topStack = stack.stack.slice(0, maxStack);
       const rest = stack.stack.slice(maxStack);
       return (
         <div
@@ -93,12 +95,12 @@ const Square = ({ game, row, col, handleSquareClick }) => {
           onClick={() => { handleSquareClick(col, row); }}
         >
           {renderSelected()}
-          {top.map((x, i) =>
+          {topStack.map((x, i) =>
              (i === 0 ?
-               <div className={`p${x} ${stack.stone} stone top`} style={topStyle}>
+               <div className={`p${x} ${stack.stone} stone ${top}`} style={topStackStyle}>
                  {/* <p className="non-flat">{` ${stack.stone} `}</p> */}
                </div> :
-               <div className={`p${x} stone captive`} style={{ ...stoneStyle, 'z-index': `${999 - i}` }} />))
+               <div className={`p${x} stone captive`} style={{ ...stoneStyle, 'z-index': `${10 - i}` }} />))
           }
           <div className="stack-overflow" style={stackOverflowStyle}>
             {rest.map(x => <div className={`p${x} stone overflow-stone`} />)}
