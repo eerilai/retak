87293b0ea3869227dfcd36411c8af419fbec842a
eerilai <eerilaie@gmail.com>
2018-09-13 16:03:34 -0500

    Style updates to reducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -4,21 +4,22 @@ import action from '../actions/actions';
 import { LOGGEDIN, LOGGEDOUT, TOGGLE_LOGIN_LOGOUT, SET_ANON_USERNAME, UPDATE_USER_PROFILE, SET_UPDATED_USERNAME, SET_CORR_GAMES } from '../actions/types';
 
 const rootReducer = (state = initialState, action) => {
-   switch (action.type) {
-    case LOGGEDIN:
-      let currentUserInfo = action.payload;
+  switch (action.type) {
+    case LOGGEDIN: {
+      const currentUserInfo = action.payload;
       return {
-        ...state, 
+        ...state,
         userID: currentUserInfo.userID,
         currentUsername: currentUserInfo.currentUsername,
         userEmail: currentUserInfo.userEmail,
         rankedGames: currentUserInfo.rankedGames,
         rankedWins: currentUserInfo.rankedWins,
         rankedLosses: currentUserInfo.rankedLosses,
-        totalGames: currentUserInfo.totalGames
+        totalGames: currentUserInfo.totalGames,
       };
-    case LOGGEDOUT:
-      let resetUserInfo = action.payload;
+    }
+    case LOGGEDOUT: {
+      const resetUserInfo = action.payload;
       return {
         ...state,
         userID: resetUserInfo.userID,
@@ -27,39 +28,45 @@ const rootReducer = (state = initialState, action) => {
         rankedGames: resetUserInfo.rankedGames,
         rankedWins: resetUserInfo.rankedWins,
         rankedLosses: resetUserInfo.rankedLosses,
-        totalGames: resetUserInfo.totalGames
+        totalGames: resetUserInfo.totalGames,
       };
-    case TOGGLE_LOGIN_LOGOUT:
-      return { 
-        ...state, 
-        isLoggedIn: action.payload
-      };
-    case SET_ANON_USERNAME:
-      let username = action.payload;
+    }
+    case TOGGLE_LOGIN_LOGOUT: {
       return {
         ...state,
-        currentUsername: username
+        isLoggedIn: action.payload,
       };
-    case UPDATE_USER_PROFILE:
-      let newUserInfo = action.payload;
+    }
+    case SET_ANON_USERNAME: {
+      const username = action.payload;
       return {
         ...state,
-        currentUsername: newUserInfo
+        currentUsername: username,
       };
-    case SET_UPDATED_USERNAME:
-      let updatedUsername = action.payload;
+    }
+    case UPDATE_USER_PROFILE: {
+      const newUserInfo = action.payload;
       return {
         ...state,
-        currentUsername: updatedUsername
+        currentUsername: newUserInfo,
       };
-    case SET_CORR_GAMES:
-    return {
-      ...state,
-      games: action.payload
+    }
+    case SET_UPDATED_USERNAME: {
+      const updatedUsername = action.payload;
+      return {
+        ...state,
+        currentUsername: updatedUsername,
+      };
+    }
+    case SET_CORR_GAMES: {
+      return {
+        ...state,
+        games: action.payload,
+      };
     }
     default:
       return state;
-   }
+  }
 };
 
 export default rootReducer;
