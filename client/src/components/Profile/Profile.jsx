5ea48759bffd9ea96d40332600dbcf89fa28d70b
eerilai <eerilaie@gmail.com>
2018-11-23 18:08:47 -0600

    Changes utilization of index.js

---
+++ client/src/components/Profile/Profile.jsx
@@ -0,0 +1,98 @@
+import React, { Component } from 'react';
+import { Route, withRouter } from 'react-router-dom';
+import { connect } from "react-redux";
+import axios from 'axios';
+import { Grid, Menu, Segment } from 'semantic-ui-react'
+import { userInfo } from 'os';
+import UserHistory from './UserHistory';
+import PageNotFound from '../PageNotFound';
+class Profile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedFile: null,
+      userHistory: [],
+      userInfo: {},
+      isUser: false
+    }
+
+    this.getUserHistory();
+    this.getUserInfo();
+
+    // axios.post('/users/:username/data', {
+    //   imageName: 'cat',
+    //   imageFile: cat
+    // })
+    //   .then(function (response) {
+    //     console.log(response);
+    //   })
+    //   .catch(function (error) {
+    //     console.log(error);
+    //   })
+  }
+
+  getUserHistory() {
+    const { userName } = this.props.match.params;
+    axios.get(`/users/${userName}/games`)
+      .then((userGameHistory) => {
+        if (userGameHistory.data !== null) {
+          this.setState({ isUser: true });
+          this.setState({ userHistory: userGameHistory.data });
+        } else {
+          this.setState({ isUser: false });
+        }
+      })
+      .catch(err => console.error(err));
+  }
+
+  getUserInfo() {
+    const { userName } = this.props.match.params;
+    axios.get(`/users/${userName}/data`)
+      .then((userInfo) => {
+        if (userInfo.data !== null) {
+          this.setState({ isUser: true });
+          this.setState({ userInfo: userInfo.data });
+        } else {
+          this.setState({ isUser: false });
+        }
+      })
+      .catch(err => console.error(err));
+  }
+
+
+
+
+
+  render() {
+    const { isLoggedIn, currentUsername, userID } = this.props;
+    const { userName } = this.props.match.params;
+
+    const userProfile = (
+      <div>
+        <UserHistory userHistory={this.state.userHistory} />
+      </div>
+    );
+
+    const notFoundPage = (
+      <PageNotFound />
+    );
+
+    return (
+      <div className="retak">
+        <div className="main">
+          {this.state.isUser ? userProfile : notFoundPage}
+        </div>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    isLoggedIn: state.isLoggedIn,
+    currentUsername: state.currentUsername,
+    userID: state.userID
+  };
+}
+
+export default withRouter(connect(mapStateToProps)(Profile));
